20e578486ee851a790a491163c80bf8e034bfc6d
Måns Dahlström <mans@mansdahlstrom.se>
2019-10-04 20:27:15 +0200

    Sorry otto

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\OttoF\Documents\Flutter apps\humidify_flutter"
-export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_ROOT=/Users/mansdahlstrom/Projects/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/mansdahlstrom/Projects/hungry_plants"
+export "FLUTTER_TARGET=/Users/mansdahlstrom/Projects/hungry_plants/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build\ios"
-export "FLUTTER_FRAMEWORK_DIR=C:\flutter\bin\cache\artifacts\engine\ios"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/mansdahlstrom/Projects/flutter/bin/cache/artifacts/engine/ios"
